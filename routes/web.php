1b0617917f239eee96980dfed400ba1e31a5e2f4
João Vagmacker <vagmackerdsm@gmail.com>
2016-10-15 10:34:41 -0300

    OAuth server com JWT e autorização finalizada

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,36 @@
 |
 */
 
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    $query = http_build_query([
+        'client_id' => 3,
+        'redirect_uri' => 'http://localhost:8087/callback',
+        'response_type' => "code",
+        'scope'=> ''
+    ]);
+
+    return redirect("http://localhost:8000/oauth/authorize?$query");
+});
+
+Route::get('callback', function(Request $request) {
+    $code = $request->get('code');
+
+    $http = new Client(); /* \GuzzleHttp\Client */
+
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => 3,
+            'client_secret' => 'hEP3weXYiYbKeTydEkzmm8Wso5R283nuz1bZa4q9',
+            'redirect_uri' => 'http://localhost:8087/callback',
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ]
+    ]);
+
+    dd(json_decode($response->getBody(), true));
 });
 
 Auth::routes();
